ca642f34b5e5ee7011aaded2dff75e6b5701f944
heitor-jsr <heitorscalon@gmail.com>
2023-01-05 20:48:14 -0300

    api done

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,18 @@
+import path from 'node:path';
 import express from 'express';
+import mongoose from 'mongoose';
+import { router } from './routes';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        const app = express();
+        app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+        app.use(express.json());
+        app.use(router);
+        app.listen(3001, () => {
+            console.log('🚀 Server is running on http://localhost:3001');
+        });
+    })
+    .catch(() => console.log('erro'));
 
-app.listen(3001, () => {
-    console.log('🚀 Server is running on http://localhost:3001');
-});
+
---
+++ api/src/routes.ts
@@ -0,0 +1,45 @@
+import { Router } from 'express';
+import multer from 'multer';
+import path from 'node:path';
+import { createCategory } from './app/useCases/categories/createCategory';
+import { listCategories } from './app/useCases/categories/listCategories';
+import { listProductsByCategories } from './app/useCases/categories/listProductsByCategories';
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { createOrder } from './app/useCases/orders/createOrder';
+import { deleteOrder } from './app/useCases/orders/deleteOrder';
+import { listOrders } from './app/useCases/orders/listOrders';
+import { createProducts } from './app/useCases/products/createProduct';
+import { listProducts } from './app/useCases/products/listProducts';
+
+export const router = Router();
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, callback) {
+            callback(null, path.resolve(__dirname,'..', 'uploads'));
+        },
+        filename(req, file, callback) {
+            callback(null, `${Date.now()}-${file.originalname}`);
+        },
+    }),
+});
+
+router.get('/categories', listCategories);
+
+router.post('/categories', createCategory);
+
+router.get('/products', listProducts);
+
+router.post('/products', upload.single('image'), createProducts);
+
+router.get('/categories/:categoryId/products', listProductsByCategories);
+
+router.get('/orders', listOrders);
+
+router.post('/orders', createOrder);
+
+router.patch('/orders/:orderId', changeOrderStatus);
+
+router.delete('/orders/:orderId', deleteOrder);
+
+
